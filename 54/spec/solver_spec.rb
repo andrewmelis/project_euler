efe7b3edcd029c393454872628a7d5dfec0b40a6
Andrew Melis <amelis@pathf.com>
2014-04-30 16:02:08 -0500

    added better test description for solver

--- 54/spec/solver_spec.rb
+++ 54/spec/solver_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe Solver do
   describe "#initialize" do
-    it "correctly counts 376" do
-      expect(Solver.new('./lib/poker.txt').p1_win_count).to eq(376)
+    context "project euler problem #54" do
+      it "correctly counts 376" do
+	expect(Solver.new('./lib/poker.txt').p1_win_count).to eq(376)
+      end
     end
   end
 end
